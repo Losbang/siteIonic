fc4d3ecf968822036653491cdb6f9902f09c452c
Losbang <contact@arthurmichel.com>
2019-11-21 14:40:46 +0100

    Edit route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'modal-coordonnees',
     loadChildren: () => import('./modal-coordonnees/modal-coordonnees.module').then( m => m.ModalCoordonneesPageModule)
